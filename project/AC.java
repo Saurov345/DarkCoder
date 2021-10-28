683d42f22eb385fdd98d404f366de40ec9a7b1d5
Saurov Karmokar <91967120+Saurov345@users.noreply.github.com>
2021-10-28 23:11:14 +0600

    Update AC.java

--- project/AC.java
+++ project/AC.java
@@ -21,30 +21,4 @@ public class AC {
 }
 
 
-class Temperature {
-    Scanner sc = new Scanner(System.in);
-    double temperature, celsius, fahrenheit, kelvin;
-    DecimalFormat f = new DecimalFormat("##.##");
- 
-    Temperature() {
-        System.out.println("\t Welcome to Temperature Converter!\t\n");
-        System.out.println("\tMain menu of Temperature conversion.\t");
-        System.out.println("\t----------------------------------");
-        System.out.println("1:Celsius.....\n2:Fahrenheit....... \n3:Kelvin.........");
-        System.out.println("Enter your choice: ");
-        int a = sc.nextInt();
-        switch (a) {
-        case 1:
-            Celsius();
-            break;
-        case 2:
-            Fahrenheit();
-            break;
-        case 3:
-            Kelvin();
-            break;
-        default: {
-            System.out.println("Wrong input. Please try again.");
-        }
-        }
-    }
+
